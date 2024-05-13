6ea9c9f0b13aeed681c7becc295c3818580bbd09
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-13 19:59:36 -0400

    Create variable.tf

---
+++ loadbalancer/variable.tf
@@ -0,0 +1,13 @@
+//Class Monday May 13 2024------------------------------------------------------------------------------------------------------------------
+variable "subscription_id"{
+  type=string
+}
+variable "client_id"{
+  type=string
+}
+variable "client_secret"{
+  type=string
+}
+variable "tenant_id"{
+  type=string
+}
